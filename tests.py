8a59aa39e57f4b588fe66855ca38313671f84694
LTS <itsnhim.30042004@gmail.com>
2023-02-25 16:55:07 +0700

    python selenium run on github actions

---
+++ tests.py
@@ -0,0 +1,27 @@
+from multiprocessing.sharedctypes import Value
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By 
+import requests
+
+
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://gmail.com")
+
+
+
+# time.sleep(100)
+
+# Total_Contract = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/div[1]/span/span").text
+# print(Total_Contract)
+# elm= driver.find_element(By.XPATH,"//h5[contains(text(),'GLOBAL STATS')]")
+# elm.click()
+
+
+driver.quit()
+
